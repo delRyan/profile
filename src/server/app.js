1cdf62a3ed872cbb9ac5373788b2fc9b75198be5
Ryan Delaney <ryan.del17@gmail.com>
2015-10-03 12:37:40 -0500

    Express server successfully starts.

---
+++ src/server/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+var logger = require('morgan');
+
+var port = process.env.PORT || 7203;
+var environment = process.env.NODE_ENV;
+
+app.use(logger('dev'));
+
+console.log('Starting server on port: ' + port);
+
+app.listen(port, function() {
+    console.log('Express server listening on port ' + port);
+    console.log('\n__dirname = ' + __dirname + '\nprocess.cwd = ' + process.cwd());
+});
