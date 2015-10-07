2d4e011621b25bcb86e9ed546a853e59feec788e
Ryan Delaney <ryan.del17@gmail.com>
2015-10-06 19:34:36 -0500

    Upgraded to Gulp 4.

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,6 @@ var config = require('./gulpconfig.js')();
 var nodemon = require('gulp-nodemon');
 var $ = require('gulp-load-plugins');
 
-gulp.task('dev', ['wiredep'], function() {
-    var nodeOptions = {
-        script: 'src/server/app.js',
-        delayTime: 1,
-        env: {
-            'PORT': 7203,
-            'NODE_ENV': 'dev'
-        }
-    };
-
-    return nodemon(nodeOptions);
-});
-
 gulp.task('wiredep', function() {    
     var options = config.getWiredepOptions;
     var wiredep = require('wiredep').stream;
@@ -26,3 +13,16 @@ gulp.task('wiredep', function() {
         //.pipe($.inject(gulp.src(config.js)))
         .pipe(gulp.dest(config.client));
 });
+
+gulp.task('dev', gulp.series('wiredep', function() {
+    var nodeOptions = {
+        script: 'src/server/app.js',
+        delayTime: 1,
+        env: {
+            'PORT': 7203,
+            'NODE_ENV': 'dev'
+        }
+    };
+
+    return nodemon(nodeOptions);
+}));
