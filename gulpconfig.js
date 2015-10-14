09670a44881de99bd556d3bf10ef91bbc6b91807
Ryan Delaney <ryan.del17@gmail.com>
2015-10-14 18:28:36 -0500

    Fixed a bug where gulp inject task injected specs into index.html

--- gulpconfig.js
+++ gulpconfig.js
@@ -11,7 +11,8 @@ module.exports = function() {
         vetjs: ['./src/**/*.js',
                 './*.js',
                 '!./karma.conf.js'],
-        injectjs: client + 'app/**/*.js',
+        injectjs: [client + 'app/**/*.js',
+                   '!' + client + 'app/**/*.spec.js'],
 
         defaultPort: 7203
     };
