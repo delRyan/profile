de70d057f8c2bbeaa46f3548cbacffa71897e6fc
Ryan Delaney <ryan.del17@gmail.com>
2015-10-20 20:33:05 -0500

    Capturing temp and build.

--- .temp/templates.js
+++ .temp/templates.js
@@ -1,4 +1,4 @@
-angular.module("app").run(["$templateCache", function($templateCache) {$templateCache.put("app/app/contact/contact.html","<div>Contact info goes here!</div>");
-$templateCache.put("app/app/network/network.html","<div>Network info goes here!</div>");
-$templateCache.put("app/app/profile/profile.html","<div>Profile info goes here!</div><div>{{profile.title}}</div>");
-$templateCache.put("app/app/skills/skills.html","<div>Skills go here!</div>");}]);
+angular.module("app").run(["$templateCache", function($templateCache) {$templateCache.put("app/contact/contact.html","<div>Contact info goes here!</div>");
+$templateCache.put("app/network/network.html","<div>Network info goes here!</div>");
+$templateCache.put("app/profile/profile.html","<div>Profile info goes here!</div><div>{{profile.title}}</div>");
+$templateCache.put("app/skills/skills.html","<div>Skills go here!</div>");}]);
