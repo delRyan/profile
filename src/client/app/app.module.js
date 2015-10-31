ea553f25db268b9045a48c86d1f65f7bf7a4e996
Ryan Delaney <ryan.del17@gmail.com>
2015-10-31 14:18:15 -0500

    Added fade transition.

--- src/client/app/app.module.js
+++ src/client/app/app.module.js
@@ -2,7 +2,7 @@
 
   'use strict';
 
-  angular.module('app', ['ui.router'])
+  angular.module('app', ['ui.router', 'ngAnimate'])
     .config(function($locationProvider, $stateProvider, $urlRouterProvider) {
 
       $urlRouterProvider.otherwise('/');
